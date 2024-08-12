5d6ee052ef6975f0a27ff37d64c9c490f1e77fca
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-08-12 20:03:44 +0530

    add:usePopcorn project and build

--- usepopcorn/src/App-v1.js
+++ usepopcorn/src/App-v1.js
@@ -1,75 +1,60 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
-  // {
-  //   imdbID: "tt1375666",
-  //   Title: "Inception",
-  //   Year: "2010",
-  //   Poster:
-  //     "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  // },
-  // {
-  //   imdbID: "tt0133093",
-  //   Title: "The Matrix",
-  //   Year: "1999",
-  //   Poster:
-  //     "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  // },
-  // {
-  //   imdbID: "tt6751668",
-  //   Title: "Parasite",
-  //   Year: "2019",
-  //   Poster:
-  //     "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  // },
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
 ];
 
 const tempWatchedData = [
-  // {
-  //   imdbID: "tt1375666",
-  //   Title: "Inception",
-  //   Year: "2010",
-  //   Poster:
-  //     "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  //   runtime: 148,
-  //   imdbRating: 8.8,
-  //   userRating: 10,
-  // },
-  // {
-  //   imdbID: "tt0088763",
-  //   Title: "Back to the Future",
-  //   Year: "1985",
-  //   Poster:
-  //     "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-  //   runtime: 116,
-  //   imdbRating: 8.5,
-  //   userRating: 9,
-  // },
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
 ];
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [isLoading,setIsLoading]=useState(false);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen2, setIsOpen2] = useState(true);
 
-const KEY="f8ed9";
-
-  useEffect(function () {
-    async function fetchMovies() {
-      setIsLoading(true);
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar
-     `);
-      const data = await res.json();
-      setMovies(data.Search);
-      console.log(data.Search)
-      setIsLoading(false);
-    }
-   fetchMovies();
-
-  },[])
 
   return (
     <>
@@ -79,8 +64,7 @@ const KEY="f8ed9";
       </Navbar>
       <Main>
         <Box>
-          {setIsLoading?<Loader/>: <MovieBox movies={movies} />
-        }
+          <MovieBox movies={movies} />
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
@@ -100,14 +84,6 @@ function Navbar({ children }) {
   );
 }
 
-function Loader(){
-  return(
-    <div className="loader" >
-      <p>Loading.....</p>
-    </div>
-  )
-}
-
 function Logo() {
   return (
     <div className="logo">
@@ -120,7 +96,7 @@ function Logo() {
 function Numresults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>{movies}</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
